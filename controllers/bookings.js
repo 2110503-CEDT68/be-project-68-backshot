8a6afa05ece7d9917fea7a2b94d4205261883b5f
Theme222 <sira.tongsima@yahoo.com>
2026-03-02 13:23:41 +0700

    removed booking limit of 3

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -114,15 +114,6 @@ exports.addBooking = async (req, res, next) => {
 
     req.body.user = req.user.id;
 
-    const existedBookings = await Booking.find({ user: req.user.id });
-
-    if (existedBookings.length >= 3 && req.user.role !== "admin") {
-      return res.status(400).json({
-        success: false,
-        message: `The user with ID ${req.user.id} has already made 3 Bookings`,
-      });
-    }
-
     const booking = await Booking.create(req.body);
 
     res.status(200).json({
